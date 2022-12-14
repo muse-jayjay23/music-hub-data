16bf321c13f37bafc75e440fc70f8e4b902e1722
muse-jayjay23 <joshuamjones23@hotmail.com>
2022-12-14 14:15:09 +0000

    Connect python script to the google sheet and check user inputs are updating the sheet correctly

--- run.py
+++ run.py
@@ -12,15 +12,15 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('Music_Hub_Data')
 
+
 def get_interest_data():
     """
-    Get sales figures input from the user.
-    Run a while loop to collect a valid string of data from the user
-    via the terminal, which must be a string of 6 numbers separated
+    Get interest figures input from the user.
+    Run a while loop to collect valid data from user which must be a string of 6 numbers separated
     by commas. The loop will repeatedly request data, until it is valid.
     """
     while True:
-        print("Please enter sales data from the last market.")
+        print("Please enter interest data from this term.")
         print("Data should be six numbers, separated by commas.")
         print("Example: 10,20,30,40,50,60\n")
 
@@ -33,6 +33,7 @@ def get_interest_data():
             break
 
     return sales_data
+
 
 def validate_data(values):
     """
@@ -52,4 +53,17 @@ def validate_data(values):
 
     return True
 
-get_interest_data()
+
+def update_interest_worksheet(data):
+    """
+    Update the google sheet with the data inputted by the user
+    """
+    print("Updating interest worksheet...\n")
+    interest_worksheet = SHEET.worksheet('interest')
+    interest_worksheet.append_row(data)
+    print("Interest worksheet updated successfully!\n")
+
+
+data = get_interest_data()
+interest_data = [int(num) for num in data]
+update_interest_worksheet(interest_data)
