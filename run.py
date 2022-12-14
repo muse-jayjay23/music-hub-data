37892b7821e39cd5db8d2ffea77049e35bd5a1c3
muse-jayjay23 <joshuamjones23@hotmail.com>
2022-12-14 13:50:59 +0000

    Add a fucntion to validate data added, if it is 6 inputs and of correct values. Show error message if either of these are incorrect

--- run.py
+++ run.py
@@ -23,6 +23,24 @@ def get_interest_data():
     data_str = input("Enter your data here: \n")
 
     interest_data = data_str.split(",")
-    print(interest_data)
+    validate_data(interest_data)
+
+def validate_data(values):
+    """
+    Inside the try, converts all string values into integers.
+    If strings cannot be converted into int it will raise the error as well as
+    if there aren't exactly 6 values.
+    """
+    try:
+        [int(value) for value in values]
+        if len(values) != 6:
+            raise ValueError(
+                f"Exactly 6 values required, you provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again.\n")
+        return False
+
+    return True
 
 get_interest_data()
